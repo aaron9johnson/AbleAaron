2317612bf6b72fa2e946e727df8ecd7ae179ad1d
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-02 16:01:41 -0700

    Done

--- WordEffects/WordEffects/main.m
+++ WordEffects/WordEffects/main.m
@@ -10,15 +10,53 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
-        
+        do{
+        printf("Which Operation: ");
+        char operation;
+        fgets(&operation, 255, stdin);
         char inputChars[255];
-        printf("input a string: ");
+        printf("\ninput a string: ");
         fgets(inputChars, 255, stdin);
         printf("your string is %s\n", inputChars);
         NSString *inputString = [NSString stringWithUTF8String:inputChars];
-        NSLog(@"Input was: %@", inputString);
-        
+        inputString = [inputString substringToIndex:[inputString length]-1];//removes \n from end
+        switch ([[NSString stringWithFormat:@"%c", operation] intValue]) {
+            case 1:
+                //UPPERCASE
+                inputString = [inputString uppercaseString];
+                break;
+            case 2:
+                //lowercase
+                inputString = [inputString lowercaseString];
+                break;
+            case 3:
+                //numberize
+                printf("int number: %d\n", [inputString intValue]);
+                break;
+            case 4:
+                //canadianize
+                inputString = [NSString stringWithFormat:@"%@, eh?", inputString];
+                break;
+            case 5:
+                //respond
+                if(inputChars[[inputString length] - 1] == '?'){
+                    inputString = @"I don't know";
+                }
+                if(inputChars[[inputString length] - 1] == '!'){
+                    inputString = @"Whoa, calm down!";
+                }
+                break;
+            case 6:
+                //de-space-it
+                inputString = [inputString stringByReplacingOccurrencesOfString:@" "
+                                                 withString:@"-"];
+                break;
+                
+            default:
+                break;
+        }
+        NSLog(@"Output is: %@", inputString);
+        }while(1);
     }
     return 0;
 }
